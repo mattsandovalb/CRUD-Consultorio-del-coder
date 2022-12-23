534191bfc3d1deea891b67cffedd98c813c8a60c
Matt <mattsandovalb@gmail.com>
2022-12-23 12:55:52 +0100

    ListModel

---
+++ controller/List_Controller.php
@@ -0,0 +1,9 @@
+<?php
+
+    require_once('model/List_model.php');
+    require_once('index.php')
+
+
+
+
+?>
--- model/dbConnect.php
+++ model/dbConnect.php
@@ -3,14 +3,14 @@
 class dbConnect{
     public static function conect(){
         try{
-            $conn = new PDO('mysql:host = localhost; dbname = appintment manager', 'root', '');
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $conn->exec("SET CHARACTER SET UTF8");
+            $conect = new PDO('mysql:host = localhost; dbname = appintment manager', 'root', '');
+            $conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conect->exec("SET CHARACTER SET UTF8");
 
         }catch(Exception $e){   
             die("Error" . $e->getMessage());
             echo "Error Line" . $e->getLine();
         }
-        return $conn;
+        return $conect;
     }
 }
