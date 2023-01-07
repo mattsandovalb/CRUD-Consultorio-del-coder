27ee200841bdbd6ffbdc19404ef9c5629a6139bf
Matt <mattsandovalb@gmail.com>
2023-01-07 17:54:19 +0100

    nasunas

--- view/head.php
+++ view/head.php
@@ -9,8 +9,8 @@
     <title>Pues no mas</title>
 </head>
 <body>
-  <header class="mb-5 ">
-    <nav class="navbar navbar-expand-lg bg-body-tertiary ">
+  <header class="mb-5">
+    <nav class="navbar navbar-expand-lg text-secondary">
       <div class="container-fluid">
         <a class="navbar-brand" href="../../../Brifing_CRUD/index.php">Mewamatà</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -28,24 +28,7 @@
         </div>
       </div>
     </nav>
-  </header>
-
-  <!-- <section class="general_section">
-    <form class="general_form" method="post"  >
-      <div class="form_div">
-        <div class="mb-3">
-            <label for="exampleInputName" class="form-label">Name Team/Coder</label>
-            <input placeholder="Name" name="Name" type="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"> 
-        </div>
-        <div class="mb-3">
-            <label for="floatingTextarea2" class="mb-3">Consult</label>
-            <textarea class="form-control" name="Consult" placeholder="Consult" id="floatingTextarea2" style="height: 100px"></textarea>
-        </div>
-        <button name="submit" value="send" type="submit" class="btn btn-primary">Submit</button>
-      </div>
-    </form> 
-    </section> -->
-      
+  </header>      
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
 </body>
